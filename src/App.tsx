1aa438e48cb2a599c6309085a0b5240338be4edf
jamesives3 <james.ives94@icloud.com>
2026-01-29 18:54:04 +1000

    update App.tsx” git pull --rebase origin main ?

--- src/App.tsx
+++ src/App.tsx
@@ -1604,7 +1604,7 @@ return [minFinal, maxFinal];
                       justifyContent: "center",
                       background: "rgba(0,0,0,0.06)",
                       color: "rgba(0,0,0,0.55)",
-                      fontSize: 22,
+                      fontSize: 12,
                       fontWeight: 700,
                       letterSpacing: 0.5,
                     }}
